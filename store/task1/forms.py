c8f230b27c087f9bfaac68cf8203624d37620b50
pinkklss <shaker_04@mail.ru>
2024-11-30 15:21:49 +0700

    Delete store directory

--- store/task1/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class UserRegister(forms.Form):
-    name = forms.CharField(label='Логин', max_length=30)
-    balance = forms.IntegerField(label='Депозит')
-    age = forms.IntegerField(label='Возраст', max_value=120)
